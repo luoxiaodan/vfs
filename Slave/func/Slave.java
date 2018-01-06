d510186a353dd35f0a312472bdd3f9db16194668
xiaofeng94 <354538123@qq.com>
2018-01-06 19:44:46 +0800

    debug code with master and client

--- Slave/func/Slave.java
+++ Slave/func/Slave.java
@@ -15,98 +15,99 @@ import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import struct.ChunkInfo;
+import vfs.struct.ChunkInfo;
 
 public class Slave {
 
-	
-	public  List<ChunkInfo> chunkInfoList=new ArrayList<ChunkInfo>();
-	public  List<Integer> chunkRent=new ArrayList<Integer>();
-	public static final String CHUNK_LOG = "E:\\chunklog.txt"; 
-	public static final String CHUNK_RENT = "E:\\chunkRentlog.txt"; 
-	public static final int CHUNK_SIZE = 64*1024; // *1024;
-	public static final int UPLOAD_BUFFER_SIZE = 8*1024;
-	public static final int DOWNLOAD_BUFFER_SIZE = 8*1024;
+	public List<ChunkInfo> chunkInfoList = new ArrayList<ChunkInfo>();
+	public List<Integer> chunkRent = new ArrayList<Integer>();
+	public static final String CHUNK_LOG = "chunklog.txt";
+	public static final String CHUNK_RENT = "chunkRentlog.txt";
+	public static final int CHUNK_SIZE = 64 * 1024; // *1024;
+	public static final int UPLOAD_BUFFER_SIZE = 8 * 1024;
+	public static final int DOWNLOAD_BUFFER_SIZE = 8 * 1024;
 	public static final int SLAVE_PORT = 8888;
-	public String Slave_ip="";
-	
-	public Slave(){};
-	public String getSlaveIp(){	
+	public String Slave_ip = "";
+
+	public Slave() {
+	};
+
+	public String getSlaveIp() {
 		return Slave_ip;
 	}
-		public void IniSalve() throws IOException{		
-	
-		Slave_ip=InetAddress.getLocalHost().getHostAddress();;
-	    FileReader fileReader=null;  
-	    BufferedReader bufferedReader=null;  
-	 
+
+	public void IniSalve() throws IOException {
+
+		Slave_ip = InetAddress.getLocalHost().getHostAddress();
+		;
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+
 		File fileName = new File(CHUNK_LOG);
-		if(fileName.exists()){  
-		      try{  
-		          fileReader=new FileReader(fileName);  
-		          bufferedReader=new BufferedReader(fileReader);  
-		          try{  
-		              String read="";  
-		              while((read=bufferedReader.readLine())!=null){  
-		           //   System.out.println("line£º"+"\r\n"+read);  
-		              String[] Info=read.split(" ");
-		              ChunkInfo chunkInfo=new ChunkInfo(Integer.valueOf(Info[0]),Info[1],Integer.valueOf(Info[2]),Integer.valueOf(Info[3]),Integer.valueOf(Info[4]));
-		             chunkInfoList.add(chunkInfo);
-		            }
-		              }catch(Exception e){  
-		           e.printStackTrace();  
-		          }
-		          }catch(Exception e){  
-		         e.printStackTrace();  
-		         }finally{  
-		           if(bufferedReader!=null){  
-		              bufferedReader.close();  
-		         }  
-		          if(fileReader!=null){  
-		             fileReader.close();  
-		        }  
-		     }  	  
-		  }
-		fileName = new File(CHUNK_RENT);
-		if(fileName.exists()){
-		     String read="";  
-		     while((read=bufferedReader.readLine())!=null){  
-		           //   System.out.println("line£º"+"\r\n"+read);  
-		      chunkRent.add(Integer.valueOf(read));
-		  }
-		          
-		      
+		if (fileName.exists()) {
+			try {
+				fileReader = new FileReader(fileName);
+				bufferedReader = new BufferedReader(fileReader);
+				try {
+					String read = "";
+					while ((read = bufferedReader.readLine()) != null) {
+						// System.out.println("lineï¿½ï¿½"+"\r\n"+read);
+						String[] Info = read.split(" ");
+						ChunkInfo chunkInfo = new ChunkInfo(Integer.valueOf(Info[0]), Info[1], Integer.valueOf(Info[2]),
+								Integer.valueOf(Info[3]), Integer.valueOf(Info[4]));
+						chunkInfoList.add(chunkInfo);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (bufferedReader != null) {
+					bufferedReader.close();
+				}
+				if (fileReader != null) {
+					fileReader.close();
+				}
+			}
 		}
-		
+		fileName = new File(CHUNK_RENT);
+		if (fileName.exists()) {
+			String read = "";
+			while ((read = bufferedReader.readLine()) != null) {
+				// System.out.println("lineï¿½ï¿½"+"\r\n"+read);
+				chunkRent.add(Integer.valueOf(read));
+			}
+
+		}
+
 	}
 
-	
-	public boolean CreateChunk(JSONObject chunk) throws Exception{
-		
-		int chunkid=chunk.getInt("chunk_id");
-		boolean isRent=chunk.getBoolean("is_rent");
-		if(isRent){
+	public boolean CreateChunk(JSONObject chunk) throws Exception {
+
+		int chunkid = chunk.getInt("chunk_id");
+		boolean isRent = chunk.getBoolean("is_rent");
+		if (isRent) {
 			chunkRent.add(chunkid);
 			writeChunkRent(chunkid);
 		}
-		ChunkInfo chunkInfo=new ChunkInfo(chunkid,Slave_ip,SLAVE_PORT,0,CHUNK_SIZE);			
+		ChunkInfo chunkInfo = new ChunkInfo(chunkid, Slave_ip, SLAVE_PORT, 0, CHUNK_SIZE);
 		chunkInfoList.add(chunkInfo);
-		JSONArray copyid=chunk.getJSONArray("ids_of_copies");
-		for (int i=0;i<copyid.length();i++){
-			chunkInfo=new ChunkInfo((int)copyid.get(i),Slave_ip,SLAVE_PORT,0,CHUNK_SIZE);			
+		JSONArray copyid = chunk.getJSONArray("ids_of_copies");
+		for (int i = 0; i < copyid.length(); i++) {
+			chunkInfo = new ChunkInfo((int) copyid.get(i), Slave_ip, SLAVE_PORT, 0, CHUNK_SIZE);
 			chunkInfoList.add(chunkInfo);
 		}
-		
+
 		this.writeChunkLog(chunkInfo);
 		return true;
 	}
-	
-	
-	public JSONArray getChunkList(){
+
+	public JSONArray getChunkList() {
 		JSONArray chunkArray = new JSONArray();
-		for(int i=0;i<chunkInfoList.size();i++){
-			
-		    ChunkInfo chunkInfo=chunkInfoList.get(i);
+		for (int i = 0; i < chunkInfoList.size(); i++) {
+
+			ChunkInfo chunkInfo = chunkInfoList.get(i);
 			JSONObject obj = new JSONObject();
 			obj.put("chunk_id", chunkInfo.chunkId);
 			obj.put("slave_ip", chunkInfo.slaveIP);
@@ -114,155 +115,148 @@ public class Slave {
 			obj.put("file_index", chunkInfo.fileIndex);
 			obj.put("chunk_left", chunkInfo.chunkLeft);
 			chunkArray.put(obj);
-		
+
 		}
 		return chunkArray;
 	}
-	
-	
-	public boolean deleteChunk(int chunkid) throws Exception{
-		boolean flagchunk=false;
-		boolean flagchunkid=false;
-		for(int i=0;i<chunkInfoList.size();i++){
-			ChunkInfo chunkInfo=chunkInfoList.get(i);
-			if (chunkInfo.chunkId==chunkid){
+
+	public boolean deleteChunk(int chunkid) throws Exception {
+		boolean flagchunk = false;
+		boolean flagchunkid = false;
+		for (int i = 0; i < chunkInfoList.size(); i++) {
+			ChunkInfo chunkInfo = chunkInfoList.get(i);
+			if (chunkInfo.chunkId == chunkid) {
 				chunkInfoList.remove(i);
-				if(chunkRent.contains(chunkid)){
-					for(int j=0;j<chunkRent.size();j++){
-						if (chunkRent.get(j)==chunkid){
+				if (chunkRent.contains(chunkid)) {
+					for (int j = 0; j < chunkRent.size(); j++) {
+						if (chunkRent.get(j) == chunkid) {
 							chunkRent.remove(j);
-							flagchunkid=true;
+							flagchunkid = true;
 							break;
 						}
 					}
 				}
-				flagchunk=true;
+				flagchunk = true;
 				break;
 			}
 		}
-		if(flagchunk){
-		for(int i=0;i<chunkInfoList.size();i++){
-			ChunkInfo chunkInfo=chunkInfoList.get(i);			
-			 File fileName = new File(CHUNK_LOG);
-			   if(fileName.exists()){ 
-				   fileName.delete();
-			   }
-			this.writeChunkLog(chunkInfo);
+		if (flagchunk) {
+			for (int i = 0; i < chunkInfoList.size(); i++) {
+				ChunkInfo chunkInfo = chunkInfoList.get(i);
+				File fileName = new File(CHUNK_LOG);
+				if (fileName.exists()) {
+					fileName.delete();
+				}
+				this.writeChunkLog(chunkInfo);
+			}
 		}
-		}
-		if(flagchunkid){
-			for(int i=0;i<chunkRent.size();i++){							
-				 File fileName = new File(CHUNK_RENT);
-				   if(fileName.exists()){ 
-					   fileName.delete();
-				   }
+		if (flagchunkid) {
+			for (int i = 0; i < chunkRent.size(); i++) {
+				File fileName = new File(CHUNK_RENT);
+				if (fileName.exists()) {
+					fileName.delete();
+				}
 				writeChunkRent(chunkRent.get(i));
 			}
-			}
+		}
 		return flagchunk;
-		
+
 	}
-	public byte[] readChunk(int chunkid,int offset,int readLen) throws IOException{
-		byte[] buffer = new byte[readLen]; 
-		for(int i=0;i<chunkInfoList.size();i++){
-			ChunkInfo chunkInfo=chunkInfoList.get(i);
-			if (chunkInfo.chunkId==chunkid){
-				
-				String contentPath="E:\\content"+Integer.toString(chunkid);
+
+	public byte[] readChunk(int chunkid, int offset, int readLen) throws IOException {
+		byte[] buffer = new byte[readLen];
+		for (int i = 0; i < chunkInfoList.size(); i++) {
+			ChunkInfo chunkInfo = chunkInfoList.get(i);
+			if (chunkInfo.chunkId == chunkid) {
+
+				String contentPath = "E:\\content" + Integer.toString(chunkid);
 				File fileName = new File(contentPath);
-				if(fileName.exists()){  
-					FileInputStream in = new FileInputStream(contentPath);  
-			         			        
-					 in.read(buffer,offset,readLen);	         
-			          
-			        in.close();  
-			        
+				if (fileName.exists()) {
+					FileInputStream in = new FileInputStream(contentPath);
+
+					in.read(buffer, offset, readLen);
+
+					in.close();
+
 				}
 				break;
 			}
 		}
-		return buffer;  
+		return buffer;
 	}
-	
-	
-	public  boolean writeChunk(int chunkid,int offset,int writeLen,String content){
-		boolean flag=false;
-		String contentPath="E:\\content"+Integer.toString(chunkid);
-		try{  
-			  File fileName = new File(contentPath);
-		   if(!fileName.exists()){  
-		    fileName.createNewFile();  
-		    System.out.println("CreateFile"+contentPath);
-		   }  
-		   FileOutputStream out = new FileOutputStream(contentPath);	
-		   		    		
-	       out.write(content.getBytes(),offset,writeLen);
-	       out.close(); 
-		   
-		   
-		  }catch(Exception e){  
-		   e.printStackTrace();  
-		  }  
-		for(int i=0;i<chunkInfoList.size();i++){
-			ChunkInfo chunkInfo=chunkInfoList.get(i);
-			if (chunkInfo.chunkId==chunkid){
-				
-				chunkInfo.chunkLeft-=writeLen;
-				
-				flag=true;
+
+	public boolean writeChunk(int chunkid, int offset, int writeLen, String content) {
+		boolean flag = false;
+		String contentPath = "E:\\content" + Integer.toString(chunkid);
+		try {
+			File fileName = new File(contentPath);
+			if (!fileName.exists()) {
+				fileName.createNewFile();
+				System.out.println("CreateFile" + contentPath);
+			}
+			FileOutputStream out = new FileOutputStream(contentPath);
+
+			out.write(content.getBytes(), offset, writeLen);
+			out.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for (int i = 0; i < chunkInfoList.size(); i++) {
+			ChunkInfo chunkInfo = chunkInfoList.get(i);
+			if (chunkInfo.chunkId == chunkid) {
+
+				chunkInfo.chunkLeft -= writeLen;
+
+				flag = true;
 				break;
 			}
-			
+
 		}
 		return flag;
 	}
-	
-	public  void writeChunkLog(ChunkInfo chunkInfo)throws Exception{  
-		 
-		  try{  
-			  File fileName = new File(CHUNK_LOG);
-		   if(!fileName.exists()){  
-		    fileName.createNewFile();  
-		    System.out.println("CreateFile"+CHUNK_LOG);
-		   }  
-		   
-		   BufferedWriter output = new BufferedWriter(new FileWriter(fileName,true));  
-           
-		   String Info=chunkInfo.chunkId+" "+chunkInfo.slaveIP+" "+chunkInfo.port+" "+chunkInfo.fileIndex+" "+chunkInfo.chunkLeft+"\n";
-		   
-		   output.write(Info);  
-           output.close(); 
-		   
-		   
-		  }catch(Exception e){  
-		   e.printStackTrace();  
-		  }  
+
+	public void writeChunkLog(ChunkInfo chunkInfo) throws Exception {
+
+		try {
+			File fileName = new File(CHUNK_LOG);
+			if (!fileName.exists()) {
+				fileName.createNewFile();
+				System.out.println("CreateFile" + CHUNK_LOG);
+			}
+
+			BufferedWriter output = new BufferedWriter(new FileWriter(fileName, true));
+
+			String Info = chunkInfo.chunkId + " " + chunkInfo.slaveIP + " " + chunkInfo.port + " " + chunkInfo.fileIndex
+					+ " " + chunkInfo.chunkLeft + "\n";
+
+			output.write(Info);
+			output.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-		  public  void writeChunkRent(int chunkid)throws Exception{  
-				 
-			  try{  
-				  File fileName = new File(CHUNK_RENT);
-			   if(!fileName.exists()){  
-			    fileName.createNewFile();  
-			    System.out.println("CreateFile"+CHUNK_RENT);
-			   }  
-			   
-			   BufferedWriter output = new BufferedWriter(new FileWriter(fileName,true));  
-	           
-			   String Info=chunkid+"\n";
-			   output.write(Info);  
-	           output.close(); 
-			   
-			   
-			  }catch(Exception e){  
-			   e.printStackTrace();  
-			  }  
-			  
-		  
-		 }   
-	
-	
-	
-	
-	
+
+	public void writeChunkRent(int chunkid) throws Exception {
+
+		try {
+			File fileName = new File(CHUNK_RENT);
+			if (!fileName.exists()) {
+				fileName.createNewFile();
+				System.out.println("CreateFile" + CHUNK_RENT);
+			}
+
+			BufferedWriter output = new BufferedWriter(new FileWriter(fileName, true));
+
+			String Info = chunkid + "\n";
+			output.write(Info);
+			output.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
--- Slave/socket/SlaveServer.java
+++ Slave/socket/SlaveServer.java
@@ -11,245 +11,245 @@ import java.net.Socket;
 
 import func.Slave;
 
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-import struct.VSFProtocols;
+import vfs.struct.VSFProtocols;
 
 public class SlaveServer {
 	private static ServerSocket server;
-	
+
 	/**
-	* ³õÊ¼»¯socket·þÎñ
-	*/
-	public static String signObj=null;
-	public static String signWork="end";
-	private static Slave slave=new Slave();
-	public static void main(String[] args) throws IOException {    
-		SlaveServer slaveServer=new SlaveServer();
+	 * ï¿½ï¿½Ê¼ï¿½ï¿½socketï¿½ï¿½ï¿½ï¿½
+	 */
+	public static String signObj = null;
+	public static String signWork = "end";
+	private static Slave slave = new Slave();
+
+	public static void main(String[] args) throws IOException {
+		SlaveServer slaveServer = new SlaveServer();
 		slave.IniSalve();
 		PushBlockQueue.getInstance().start();
 		slaveServer.Server();
-	    }
-		
-	    public  void Server() throws IOException{
-	    	slave.IniSalve();
-	    	 try {    
-	             ServerSocket serverSocket = new ServerSocket(Slave.SLAVE_PORT);    
-	             while (true) {    
-	                 
-	                 Socket client = serverSocket.accept();    
-	                  
-	                 new HandlerThread(client);    
-	             }    
-	         } catch (Exception e) {    
-	             System.out.println("server error: " + e.getMessage());    
-	         }    
-	    }
-	    
-	    private class HandlerThread implements Runnable {    
-	        private Socket socket;    
-	        public HandlerThread(Socket client) {    
-	            socket = client;    
-	            new Thread(this).start();    
-	        }
-			@Override
-			public void run() {
-				try {   
-	
-	 		       int protocols=inputProtocols(socket);		
-				  
-				   if((protocols==VSFProtocols.WRITE_CHUNK)||(protocols==VSFProtocols.READ_CHUNK)){
-					   System.out.println("put");
-					   PushBlockQueue.getInstance().put(String.valueOf(protocols)+":"+socket.getPort());
-				   }else{
-					   
-					   if((protocols==VSFProtocols.INITIALIZE_CHUNK_INFO)||(protocols==VSFProtocols.NEW_CHUNK)||(protocols==VSFProtocols.RELEASE_CHUNK)){ //master   
-						  
-						   switch (protocols) {
-			                case VSFProtocols.INITIALIZE_CHUNK_INFO://chunkinfo
-			                    getChunkInfo(socket);
-			                    break;
-	
-			                case VSFProtocols.NEW_CHUNK://createchunk
-			                    createChunk(socket);
-			                    break;	
-			                case VSFProtocols.RELEASE_CHUNK:
-			                	deleteChunk(socket);
-			                	break;
-			                }					   
-						   
-					   }else{
-					   
-						   responesClient(socket,"Please wait...");
-					   }
-				   }
-				   Thread.sleep(500);
-				   System.out.println("context signObj :"+protocols+" "+signObj);
-				   if(signObj.equals(new String(protocols+":"+socket.getPort()))){
-					  
-					    
-		        		String[] Info=signObj.split(":");
-		        		responesClient(socket,Info[0]);
-		        		switch (Integer.valueOf(Info[0])) {
-		                case VSFProtocols.WRITE_CHUNK:
-		                    writeChunk(socket);
-		                    break;
-	
-		                case VSFProtocols.READ_CHUNK:
-		                    readChunk(socket);
-		                    break;
-		                
-		                }
-					   
-				   
-				   
-				
-			}
-			// server.close();
-			} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("error : " + e.getMessage());
-			}
-			}
-	
-	public void writeChunk(Socket socket) throws IOException{
-		  
-            
-              DataInputStream input = new DataInputStream(socket.getInputStream());  
-      	      int chunkid = inputInt(socket);//chunk_id        	  
-        	  int offset= inputInt(socket);//offset       	  
-        	  int writelen = inputInt(socket);//writelen
-        	  String contentBuff="";
-       	 
-        	  //content
-        	  int contentCount = 0;
-        	  byte[] tempBuf = new byte[Slave.UPLOAD_BUFFER_SIZE];
-          	
-          	  while(true){
-          		if(contentCount >= writelen){
-          			break;
-          		}
-          		int cRead = Math.min(writelen - contentCount, tempBuf.length);
-          		int aRead = input.read(tempBuf, 0, cRead);          			
-          			contentBuff+= new String(tempBuf);        		
-          		    contentCount += aRead;
-          	}
-        	
-        	  
-              boolean stateWruteChunk=slave.writeChunk(chunkid,offset,writelen,contentBuff);
-              signWork="end";              
-              responseStatus(socket,stateWruteChunk);
-            
-	
 	}
 
-    public void readChunk(Socket socket) throws NumberFormatException, IOException{
-	        DataOutputStream out = new DataOutputStream(socket.getOutputStream());				    
-	        int chunkid = inputInt(socket);//chunk_id        	  
-      	    int offset= inputInt(socket);//offset       	  
-      	    int readlen = inputInt(socket);//readlen
-		    byte[] content=slave.readChunk(chunkid,offset,readlen);		    				
-		    responesClient(socket,"OK");
-          
-            out.writeInt(readlen);
-	      
-	    	int bufferSize = Slave.UPLOAD_BUFFER_SIZE;
+	public void Server() throws IOException {
+		slave.IniSalve();
+		try {
+			ServerSocket serverSocket = new ServerSocket(Slave.SLAVE_PORT);
+			while (true) {
+
+				Socket client = serverSocket.accept();
+
+				new HandlerThread(client);
+			}
+		} catch (Exception e) {
+			System.out.println("server error: " + e.getMessage());
+		}
+	}
+
+	private class HandlerThread implements Runnable {
+		private Socket socket;
+
+		public HandlerThread(Socket client) {
+			socket = client;
+			new Thread(this).start();
+		}
+
+		@Override
+		public void run() {
+			System.out.println("start running");
+			try {
+
+				int protocols = inputProtocols(socket);
+				System.out.println("protocol id" + protocols);
+
+				if ((protocols == VSFProtocols.WRITE_CHUNK) || (protocols == VSFProtocols.READ_CHUNK)) {
+					System.out.println("put");
+					// PushBlockQueue.getInstance().put(String.valueOf(protocols)
+					// + ":" + socket.getPort());
+					String[] Info = signObj.split(":");
+					responesClient(socket, Info[0]);
+					switch (Integer.valueOf(Info[0])) {
+					case VSFProtocols.WRITE_CHUNK:
+						writeChunk(socket);
+						break;
+
+					case VSFProtocols.READ_CHUNK:
+						readChunk(socket);
+						break;
+
+					}
+
+				} else {
+
+					if ((protocols == VSFProtocols.INITIALIZE_CHUNK_INFO) || (protocols == VSFProtocols.NEW_CHUNK)
+							|| (protocols == VSFProtocols.RELEASE_CHUNK)) { // master
+
+						switch (protocols) {
+						case VSFProtocols.INITIALIZE_CHUNK_INFO:// chunkinfo
+							getChunkInfo(socket);
+							break;
+
+						case VSFProtocols.NEW_CHUNK:// createchunk
+							createChunk(socket);
+							break;
+						case VSFProtocols.RELEASE_CHUNK:
+							deleteChunk(socket);
+							break;
+						}
+						// PushBlockQueue.getInstance().put(String.valueOf(protocols)
+						// + ":" + socket.getPort());
+
+					} else {
+
+						responesClient(socket, "Please wait...");
+					}
+				}
+				Thread.sleep(500);
+
+				// server.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("error : " + e.getMessage());
+			}
+		}
+
+		public void writeChunk(Socket socket) throws IOException {
+
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			int chunkid = inputInt(socket);// chunk_id
+			int offset = inputInt(socket);// offset
+			int writelen = inputInt(socket);// writelen
+			String contentBuff = "";
+
+			// content
+			int contentCount = 0;
+			byte[] tempBuf = new byte[Slave.UPLOAD_BUFFER_SIZE];
+
+			while (true) {
+				if (contentCount >= writelen) {
+					break;
+				}
+				int cRead = Math.min(writelen - contentCount, tempBuf.length);
+				int aRead = input.read(tempBuf, 0, cRead);
+				contentBuff += new String(tempBuf);
+				contentCount += aRead;
+			}
+
+			boolean stateWruteChunk = slave.writeChunk(chunkid, offset, writelen, contentBuff);
+			signWork = "end";
+			responseStatus(socket, stateWruteChunk);
+
+		}
+
+		public void readChunk(Socket socket) throws NumberFormatException, IOException {
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			int chunkid = inputInt(socket);// chunk_id
+			int offset = inputInt(socket);// offset
+			int readlen = inputInt(socket);// readlen
+			byte[] content = slave.readChunk(chunkid, offset, readlen);
+			responesClient(socket, "OK");
+
+			out.writeInt(readlen);
+
+			int bufferSize = Slave.UPLOAD_BUFFER_SIZE;
 			byte[] contentBuff = new byte[bufferSize];
 			int contentCount = 0;
-			while(contentCount < readlen){
-				int writeNum = Math.min(bufferSize, readlen-contentCount);
-				for(int i = 0; i < bufferSize; ++i){
-					contentBuff[i] = content[contentCount+i];
+			while (contentCount < readlen) {
+				int writeNum = Math.min(bufferSize, readlen - contentCount);
+				for (int i = 0; i < bufferSize; ++i) {
+					contentBuff[i] = content[contentCount + i];
 				}
 				out.write(contentBuff, 0, writeNum);
 				out.flush();
-				
-				contentCount += writeNum;
-			}               
-	        out.close(); 
-    }
-    
-    public void getChunkInfo(Socket socket) throws IOException{
-    	DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-    	JSONArray chunkarray=slave.getChunkList();
-    	String string=chunkarray.toString();
-        
-        byte[] bytes=string.getBytes();
-       
-        out.writeInt(bytes.length);
-		out.write(bytes, 0, bytes.length);
-        out.close();
-    }
-    
-    public void deleteChunk(Socket socket) throws NumberFormatException, Exception{
-    	//doing
-    	BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    	boolean stateWruteChunk=slave.deleteChunk(Integer.valueOf(in.readLine()));
-    	responseStatus(socket,stateWruteChunk);
-    }
-    
-    public void createChunk(Socket socket) throws Exception{
-        DataInputStream input = new DataInputStream(socket.getInputStream());
-        int length = input.readInt();
-    	byte[] bytes = new byte[length];
-    	input.read(bytes, 0, length);
-    	
-        JSONObject chunk = new JSONObject(new String(bytes));
-		boolean stateWruteChunk=slave.CreateChunk(chunk);
-		responseStatus(socket,stateWruteChunk);
-        
-    }
-    
-    public void responesClient(Socket socket,String content) throws IOException{
-    	   DataOutputStream out = new DataOutputStream(socket.getOutputStream());    
-    	   out.writeUTF(content);
-		   System.out.println("response to client: " + content);
- 		   out.close();
-    }
-    
-    public void responseStatus(Socket socket,boolean check) throws IOException{
-    	if(check){
-      	  responesClient(socket,VSFProtocols.MESSAGE_OK); 
-           
-        }else{
-      	  responesClient(socket,VSFProtocols.MASTER_REJECT); 
-        }
-    }
-    public  int inputProtocols(Socket socket) throws IOException {		
-    	DataInputStream input = new DataInputStream(socket.getInputStream());
-		
-    	byte[] protocolBuff = new byte[8];
-		input.read(protocolBuff, 0, 8);
-		int len = 0;
-		for (int i = 0; i < protocolBuff.length; ++i) {
-			if (protocolBuff[i] == '\0') {
-				len = i;
-				break;
-			}
-		}
-		return Integer.valueOf(new String(protocolBuff, 0, len));
-    	
-    	
-	}
-    public  int inputInt(Socket socket) throws IOException {		
-    	return Integer.valueOf(inputString(socket));
-	}
-    public  String inputString(Socket socket) throws IOException {
-		DataInputStream input = new DataInputStream(socket.getInputStream());
-		int length = input.readInt();
 
-    	byte[] string = new byte[length];
-		input.read(string, 0, length);
-		int len = 0;
-		for (int i = 0; i < string.length; ++i) {
-			if (string[i] == '\0') {
-				len = i;
-				break;
+				contentCount += writeNum;
+			}
+			out.close();
+		}
+
+		public void getChunkInfo(Socket socket) throws IOException {
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			JSONArray chunkarray = slave.getChunkList();
+			String string = chunkarray.toString();
+
+			byte[] bytes = string.getBytes();
+
+			out.writeInt(bytes.length);
+			out.write(bytes, 0, bytes.length);
+			out.close();
+		}
+
+		public void deleteChunk(Socket socket) throws NumberFormatException, Exception {
+			// doing
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			boolean stateWruteChunk = slave.deleteChunk(Integer.valueOf(in.readLine()));
+			responseStatus(socket, stateWruteChunk);
+		}
+
+		public void createChunk(Socket socket) throws Exception {
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			int length = input.readInt();
+			byte[] bytes = new byte[length];
+			input.read(bytes, 0, length);
+
+			JSONObject chunk = new JSONObject(new String(bytes));
+			boolean stateWruteChunk = slave.CreateChunk(chunk);
+			responseStatus(socket, stateWruteChunk);
+
+		}
+
+		public void responesClient(Socket socket, String content) throws IOException {
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			out.writeUTF(content);
+			System.out.println("response to client: " + content);
+			out.close();
+		}
+
+		public void responseStatus(Socket socket, boolean check) throws IOException {
+			if (check) {
+				responesClient(socket, VSFProtocols.MESSAGE_OK);
+
+			} else {
+				responesClient(socket, VSFProtocols.MASTER_REJECT);
 			}
 		}
-	    return new String(string);
-	    }
-	    }
+
+		public int inputProtocols(Socket socket) throws IOException {
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+
+			byte[] protocolBuff = new byte[8];
+			input.read(protocolBuff, 0, 8);
+			int len = 0;
+			for (int i = 0; i < protocolBuff.length; ++i) {
+				if (protocolBuff[i] == '\0') {
+					len = i;
+					break;
+				}
+			}
+			return Integer.valueOf(new String(protocolBuff, 0, len));
+
+		}
+
+		public int inputInt(Socket socket) throws IOException {
+			return Integer.valueOf(inputString(socket));
+		}
+
+		public String inputString(Socket socket) throws IOException {
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			int length = input.readInt();
+
+			byte[] string = new byte[length];
+			input.read(string, 0, length);
+			int len = 0;
+			for (int i = 0; i < string.length; ++i) {
+				if (string[i] == '\0') {
+					len = i;
+					break;
+				}
+			}
+			return new String(string);
+		}
+	}
 }
